564e988d5fb74396a092b21df5ab5eace7f3f24c
azhar_azad <azhar.u.azad@gmail.com>
2023-05-19 17:04:42 +0600

    project initialized

---
+++ src/test/java/com/azad/basicecommerce/BasicEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.basicecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
